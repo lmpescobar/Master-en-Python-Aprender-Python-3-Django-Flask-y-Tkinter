f505dfea02004cea36627172acb5e946d127882b
lmpescobar <lmpes@tutanota.com>
2025-06-19 21:00:40 -0400

    Variables

---
+++ c02_Variables y tipos de datos/p01_Variables en Python.py
@@ -0,0 +1,22 @@
+"""
+En Python, las variables son contenedores para almacenar datos en memoria. 
+A diferencia de otros lenguajes, no requieren declaración explícita de tipo 
+(tipado dinámico) y se crean al asignarles un valor.
+"""
+
+texto = "Máster en Python"
+texto2 = "Luis Peña"
+numero = 45
+decimal = 6.7
+
+print(texto)
+print(texto2)
+print(numero)
+print(decimal)
+
+print("--------------------------------")
+
+numero = 77
+decimal = 8.9
+print(numero)
+print(decimal)
